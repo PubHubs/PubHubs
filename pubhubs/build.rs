eee63f861a02fbb363584e88ac296b2f55b10ca5
Harm van Stekelenburg <h.vanstekelenburg@cs.ru.nl>
2023-05-30 16:05:41 +0200

    Resolve "Remove creation of scss and global client from build.rs"

--- pubhubs/build.rs
+++ pubhubs/build.rs
@@ -1,10 +1,12 @@
 extern crate core;
 
+use std::fs::create_dir;
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
     generate_css();
-    generate_global_client();
+    generate_global_client_folder();
 }
 
 fn generate_css() {
@@ -41,54 +43,16 @@ fn generate_css() {
     }
 }
 
-fn generate_global_client() {
-    let output_install = Command::new("npm")
-        .args(["ci", "--prefix", "../global-client"])
-        .output()
-        .expect("Expected to use npm install");
-    if !output_install.status.success() {
-        let stdout = String::from_utf8(output_install.stdout).unwrap();
-        let stderr = String::from_utf8(output_install.stderr).unwrap();
-        println!(
-            "cargo:warning={} {}{}",
-            output_install.status, stdout, stderr
-        );
-        panic!();
+fn generate_global_client_folder() {
+    let client_output_dir = Path::new("static/assets/client");
+    if client_output_dir.exists() {
+        return;
     }
 
-    let output_install = Command::new("npm")
-        .args(["ci", "--prefix", "../hub-client"])
-        .output()
-        .expect("Expected to use npm install");
-    if !output_install.status.success() {
-        let stdout = String::from_utf8(output_install.stdout).unwrap();
-        let stderr = String::from_utf8(output_install.stderr).unwrap();
-        println!(
-            "cargo:warning={} {}{}",
-            output_install.status, stdout, stderr
-        );
-        panic!();
-    }
+    let res = create_dir(client_output_dir);
 
-    let output = Command::new("npm")
-        .args(["run", "--prefix", "../global-client", "build"])
-        .output()
-        .expect("Expected to use global client build script");
-    if !output.status.success() {
-        let stdout = String::from_utf8(output.stdout).unwrap();
-        let stderr = String::from_utf8(output.stderr).unwrap();
-        println!("cargo:warning={} {}{}", output.status, stdout, stderr);
-        panic!();
-    }
-
-    let output = Command::new("cp")
-        .args(["-a", "../global-client/dist/.", "static/assets/client"])
-        .output()
-        .expect("Expected to copy the global client build output");
-    if !output.status.success() {
-        let stdout = String::from_utf8(output.stdout).unwrap();
-        let stderr = String::from_utf8(output.stderr).unwrap();
-        println!("cargo:warning={} {}{}", output.status, stdout, stderr);
+    if res.is_err() {
+        println!("cargo:warning={:?}", res.err().unwrap());
         panic!();
     }
 }
