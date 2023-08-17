77c637a01a2294e895e6901d1131b6bc2faaabdb
AA Westerbaan <a.westerbaan@science.ru.nl>
2023-08-17 14:04:09 +0200

    Resolve "Upgrade to ed25519_dalek version 2"

--- pubhubs/src/old/jwt.rs
+++ pubhubs/src/old/jwt.rs
@@ -60,29 +60,30 @@ pub trait Key {
 ///
 /// ```
 /// use pubhubs::jwt::SigningKey;
-/// use ed25519_dalek::Keypair;
 /// use base64ct::{Base64UrlUnpadded, Encoding as _};
 ///
-/// let privk = r#"9d 61 b1 9d ef fd 5a 60 ba 84 4a f4 92 ec 2c c4
-///                44 49 c5 69 7b 32 69 19 70 3b ac 03 1c ae 7f 60"#;
-/// let pubk = r#"d7 5a 98 01 82 b1 0a b7 d5 4b fe d3 c9 64 07 3a
-///               0e e1 72 f3 da a6 23 25 af 02 1a 68 f7 07 51 1a"#;
-/// let mut kphex = format!("{} {}", privk, pubk);
-/// kphex.retain(|c| !c.is_whitespace());
-/// println!("{}", kphex);
-/// let kpbytes = base16ct::lower::decode_vec(kphex.as_bytes()).unwrap();
-/// let kp = Keypair::from_bytes(&kpbytes).unwrap();
+/// let mut privhex : String = r#"9d 61 b1 9d ef fd 5a 60 ba 84 4a f4 92 ec 2c c4
+///                44 49 c5 69 7b 32 69 19 70 3b ac 03 1c ae 7f 60"#.into();
+/// let mut pubhex : String = r#"d7 5a 98 01 82 b1 0a b7 d5 4b fe d3 c9 64 07 3a
+///               0e e1 72 f3 da a6 23 25 af 02 1a 68 f7 07 51 1a"#.into();
+/// privhex.retain(|c| !c.is_whitespace());
+/// pubhex.retain(|c| !c.is_whitespace());
+/// let privkey = base16ct::lower::decode_vec(privhex.as_bytes()).unwrap();
+/// let pubkey = base16ct::lower::decode_vec(pubhex.as_bytes()).unwrap();
+/// let sk = ed25519_dalek::SigningKey::try_from(privkey.as_slice()).unwrap();
+///
+/// assert_eq!(sk.verifying_key().to_bytes().as_slice(), pubkey.as_slice());
 ///
 /// // See "A.4 Ed25519 Signing" from RFC8037.
 /// assert_eq!(
-///     Base64UrlUnpadded::encode_string(&SigningKey::sign(&kp,
+///     Base64UrlUnpadded::encode_string(&SigningKey::sign(&sk,
 ///             "eyJhbGciOiJFZERTQSJ9.RXhhbXBsZSBvZiBFZDI1NTE5IHNpZ25pbmc".as_bytes()
 ///         ).unwrap().as_ref()
 ///     ),
 ///     "hgyY0il_MGCjP0JzlnLWG1PPOt7-09PGcvMg3AIbQR6dWbhijcNR4ki4iylGjg5BhVsPt9g7sVvpAr_MuM0KAg");
 ///
 /// // See "A.3 JWK Thumbprint Canonicalization" from RFC8037.
-/// assert_eq!(kp.jwk(), serde_json::json!({
+/// assert_eq!(sk.jwk(), serde_json::json!({
 ///     "kty": "OKP",
 ///     "alg": "EdDSA",
 ///     "crv": "Ed25519",
@@ -90,11 +91,11 @@ pub trait Key {
 ///     "use": "sig",
 /// }));
 /// ```
-impl SigningKey for ed25519_dalek::Keypair {
-    type Signature = ed25519_dalek::Signature;
+impl SigningKey for ed25519_dalek::SigningKey {
+    type Signature = [u8; 64]; // ed25519_dalek::Signature no longer implements AsRef<[u8]>
 
-    fn sign(&self, s: &[u8]) -> anyhow::Result<ed25519_dalek::Signature> {
-        Ok(ed25519_dalek::Signer::sign(self, s))
+    fn sign(&self, s: &[u8]) -> anyhow::Result<[u8; 64]> {
+        Ok(ed25519_dalek::Signer::sign(self, s).to_bytes())
     }
 
     fn jwk(&self) -> serde_json::Value {
@@ -102,14 +103,14 @@ impl SigningKey for ed25519_dalek::Keypair {
             "kty": "OKP", // not "EC", see RFC8037, Section 2.
             "alg": Self::ALG,
             "crv": "Ed25519",
-            "x": Base64UrlUnpadded::encode_string(self.public.as_ref()),
+            "x": Base64UrlUnpadded::encode_string(AsRef::<ed25519_dalek::VerifyingKey>::as_ref(self).as_bytes()),
             // parameter "d" must NOT be included, being the private key
             "use": "sig",
         })
     }
 }
 
-impl Key for ed25519_dalek::Keypair {
+impl Key for ed25519_dalek::SigningKey {
     const ALG: &'static str = "EdDSA";
 }
 
