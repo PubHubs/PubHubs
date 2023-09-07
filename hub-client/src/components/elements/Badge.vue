050ea4904186f3b916c1027bc9fa1902b9f789ee
Jan den Besten <info@jandenbesten.net>
2023-09-07 15:36:26 +0200

    ‘walking’ badge solved

--- hub-client/src/components/elements/Badge.vue
+++ hub-client/src/components/elements/Badge.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="bg-red text-white absolute rounded-full text-center w-5 h-5 p-0 text-sm">
-		<span class="inline-block"><slot></slot></span>
+	<div class="relative">
+		<div class="bg-red text-white absolute rounded-full text-center w-5 h-5 p-0 text-sm">
+			<span class="inline-block"><slot></slot></span>
+		</div>
 	</div>
 </template>
