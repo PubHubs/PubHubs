5b152fcae4885d8dc1e4bb7ad8eca4039c409f72
Omar Javed <omar.javed@ru.nl>
2023-09-22 15:04:38 +0200

    fixed test failures

--- hub-client/src/core/api.ts
+++ hub-client/src/core/api.ts
@@ -7,7 +7,7 @@
 import { Api } from '@/core/apiCore';
 
 // @ts-ignore
-const api_synapse = new Api(_env.HUB_URL + '/_synapse', {
+const api_synapse = new Api(_env.HUB_URL + '/_synapse/', {
 	securedRooms: 'client/secured_rooms',
 	deleteRoom: 'admin/v2/rooms/',
 });
