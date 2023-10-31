497cee4d264df9a3496974d26eea3079700764d5
Jan den Besten <info@jandenbesten.net>
2023-10-31 11:40:47 +0100

    fix2

--- hub-client/start.sh
+++ hub-client/start.sh
@@ -1,2 +1,2 @@
 TIMESTAMP=$(date +%s)
-echo "const _env = {\"HUB_URL\": \"$HUB_URL\",\"PARENT_URL\": \"$PARENT_URL\",\"TIMESTAMP\": \"$TIMESTAMP\"}" > client-config.js
+echo "const _env = {\"HUB_URL\": \"$HUB_URL\",\"PARENT_URL\": \"$PARENT_URL\",\"TIMESTAMP\": \"$TIMESTAMP\"}" > /usr/var/static/client-config.js
