281ee3900448a04c0dd44d7942cf9419612d5a92
Muhammet Temizkan <m.temizkan@science.ru.nl>
2023-11-02 14:27:13 +0100

    Resolve "Localisation of the hubpage"

--- hub-client/src/pages/HomePage.vue
+++ hub-client/src/pages/HomePage.vue
@@ -1,68 +1,68 @@
 <template>
 	<div class="flex flex-col h-full xl:max-w-screen-xl m-auto p-5">
 		<div class="flex flex-col w-full">
-			<H3>Laatste nieuws</H3>
+			<H3>{{ $t('rooms.latest_news') }}</H3>
 			<div class="flex flex-col md:flex-row justify-between gap-4">
 				<ArticleBlock>
 					<template #header>Relevante video titel</template>
-					<template #category>Kijk</template>
+					<template #category>{{ $t('rooms.watch') }}</template>
 					<template #content>Video | 20-12-2022</template>
 				</ArticleBlock>
 				<ArticleBlock>
 					<template #header>Artikel over iets leuks</template>
-					<template #category>Lees</template>
+					<template #category>{{ $t('rooms.read') }}</template>
 					<template #content>Artikel - 28 November</template>
 				</ArticleBlock>
 				<ArticleBlock>
 					<template #header>Voorbeeld hub meetup 2022</template>
-					<template #category>Evenement</template>
+					<template #category>{{ $t('rooms.event') }}</template>
 					<template #content>9 december, Goffertpark Nijmegen</template>
 				</ArticleBlock>
 			</div>
 			<div class="flex justify-end mt-4">
-				<button class="dark:bg-gray-dark rounded-md px-3 tracking-wide">Meer nieuws →</button>
+				<button class="dark:bg-gray-dark rounded-md px-3 tracking-wide">{{ $t('rooms.more_news') }} →</button>
 			</div>
 		</div>
 		<SearchInput class="my-8"></SearchInput>
-		<H3>Populaire rooms & discussies</H3>
+		<H3>{{ $t('rooms.popular_rooms') }}</H3>
 		<div class="flex flex-col gap-4">
 			<div class="flex flex-col md:flex-row justify-between gap-4">
 				<ArticleBlock>
 					<template #header>Smartphones en jonge kinderen</template>
-					<template #category>Discussie</template>
+					<template #category>{{ $t('rooms.discussion') }}</template>
 					<template #content>20-12-2022</template>
 				</ArticleBlock>
 				<ArticleBlock>
 					<template #header>Voorbeeld discussie</template>
-					<template #category>Discussie</template>
+					<template #category>{{ $t('rooms.discussion') }}</template>
 					<template #content>20-12-2022</template>
 				</ArticleBlock>
 				<ArticleBlock>
 					<template #header>Voorbeeld room</template>
-					<template #category>Room</template>
+					<template #category>{{ $t('rooms.discussion') }}</template>
 					<template #content>20-12-2022</template>
 				</ArticleBlock>
 			</div>
 			<div class="flex flex-col md:flex-row justify-between gap-4">
 				<ArticleBlock>
 					<template #header>Smartphones en jonge kinderen</template>
-					<template #category>Discussie</template>
+					<template #category>{{ $t('rooms.discussion') }}</template>
 					<template #content>20-12-2022</template>
 				</ArticleBlock>
 				<ArticleBlock>
 					<template #header>Voorbeeld discussie</template>
-					<template #category>Discussie</template>
+					<template #category>{{ $t('rooms.discussion') }}</template>
 					<template #content>20-12-2022</template>
 				</ArticleBlock>
 				<ArticleBlock>
 					<template #header>Voorbeeld room</template>
-					<template #category>Room</template>
+					<template #category>{{ $t('rooms.discussion') }}</template>
 					<template #content>20-12-2022</template>
 				</ArticleBlock>
 			</div>
 		</div>
 		<div class="flex justify-end mt-4">
-			<button class="dark:bg-gray-dark rounded-md px-3 tracking-wide">Meer suggesties →</button>
+			<button class="dark:bg-gray-dark rounded-md px-3 tracking-wide">{{ $t('rooms.more_suggestions') }} →</button>
 		</div>
 	</div>
 </template>
