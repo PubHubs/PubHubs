6cf34850f3cc3a89493f9175ce3b40310eae12f4
Aron Schöffer <aronschoffer@gmail.com>
2023-12-14 09:27:52 +0100

    Temporarily remove signing messages for merge to stable

--- hub-client/src/components/ui/UploadPicker.vue
+++ hub-client/src/components/ui/UploadPicker.vue
@@ -5,10 +5,11 @@
     <p>{{ $t('message.upload_file') }}</p>
   </button>
 
-  <button class="flex flex-col items-center m-2" @click="$emit('sign', $event)">
+  <!-- Commented out until signed messages work -->
+  <!-- <button class="flex flex-col items-center m-2" @click="$emit('sign', $event)">
     <Icon class="dark:text-white" type="sign"></Icon>
     <p>{{ $t('message.sign.add_signature') }}</p>
-  </button>
+  </button> -->
 </div>
 </template>
 
