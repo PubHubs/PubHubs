6ae4c9561b05da3b054ad78cb1640f88ac8532bb
Aron Schöffer <a.schoffer@student.science.ru.nl>
2024-01-10 13:47:54 +0100

    add documentation explaining why --allow-unsigned-callbacks is safe to use

--- pubhubs_hub/start.sh
+++ pubhubs_hub/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Note that the irma (yivi) server will not be publicly accessible (only through a proxy hosted by the hub), so using --allow-unsigned-callbacks should be safe
 irma server --issue-perms "*" --production --no-email --no-tls --sse --allow-unsigned-callbacks --no-auth -l 0.0.0.0 -p 8089 --client-listen-addr 0.0.0.0 --client-port 8088 -vvvvvvvvv &
 
 if [ $DONT_START_HUB -eq 1 ]
