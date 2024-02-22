b022c3078843466418e1c89adef95762a1cc0a30
Jan den Besten <info@jandenbesten.net>
2024-02-22 10:07:24 +0100

    fix: global-client build error (unused import)

--- global-client/src/store/global.ts
+++ global-client/src/store/global.ts
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia';
 import { getCookie } from 'typescript-cookie';
-import { Buffer } from 'buffer';
 
 import { Hub, HubList, Theme, TimeFormat, useSettings } from '@/store/store';
 import { api } from '@/core/api';
