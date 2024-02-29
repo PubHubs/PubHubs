b30fe7fce82b692708555971218be9f3364cd3a2
Aron Schöffer <aronschoffer@gmail.com>
2024-02-29 11:10:10 +0100

    fix mention typescript error

--- hub-client/src/store/rooms.ts
+++ hub-client/src/store/rooms.ts
@@ -516,9 +516,7 @@ const useRooms = defineStore('rooms', {
 			let onlyPseudonymInLogUser = '';
 			// It is a string so better to convert it to an array of mentions.
 			// It could be a single string or an array of strings. Hencce we convert it to an array.
-			const userIdsInMention: string[] = currentMsgEvent.event.content?.['m.mentions'].user_ids.includes(',')
-				? currentMsgEvent.event.content?.['m.mentions'].user_ids.split(',')
-				: [currentMsgEvent.event.content?.['m.mentions'].user_ids];
+			const userIdsInMention = currentMsgEvent.event.content?.['m.mentions']?.user_ids || [];
 
 			const loggedInUserInMention = user.user.displayName!;
 			// XXX: Another check for handling display name with pseudonym issue.
