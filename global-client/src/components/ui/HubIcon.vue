f94cd492f02c8408078ac65257b0ca3b5f3c3b23
Aron Schöffer <aronschoffer@gmail.com>
2024-05-02 15:34:57 +0200

    add fallback for hub logo

--- global-client/src/components/ui/HubIcon.vue
+++ global-client/src/components/ui/HubIcon.vue
@@ -3,7 +3,7 @@
 		<Badge v-if="hub && hub.unreadMessages > 0" class="sm:ml-6">{{ hub.unreadMessages }}</Badge>
 		<Icon v-if="pinnable" type="plus" class="text-green absolute right-0" @click.prevent="pin"></Icon>
 		<Icon v-if="pinned" type="remove" class="text-red absolute right-0 hidden group-hover:block" @click.prevent="remove"></Icon>
-		<Icon :type="type" :size="size" class="mx-auto"></Icon>
+		<Icon :type="type" :size="size" class="text-white mx-auto"></Icon>
 		<HubLogo v-if="hub" :hub-url="hub.url" :hub-id="hub.hubId" :change-to-dark="false" class="absolute h-14 w-14 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></HubLogo>
 	</div>
 </template>
