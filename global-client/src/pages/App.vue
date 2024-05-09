d9f5e889d1bbbfe13692f26871186ee6b7d96fea
Aron Schöffer <aronschoffer@gmail.com>
2024-05-09 15:46:05 +0200

    fix formatting

--- global-client/src/pages/App.vue
+++ global-client/src/pages/App.vue
@@ -9,9 +9,9 @@
 				<div id="pubhubs-bar" class="flex-none w-32 bg-ph-background-3 dark:bg-ph-background-5 h-screen pt-20 2md:pt-2 2md:block" :class="{ hidden: !toggleMenu.globalIsActive }">
 					<Modal :show="global.isModalVisible">
 						<div class="flex flex-col justify-between h-full">
-							<div class="flex-1 text-center ">
+							<div class="flex-1 text-center">
 								<router-link to="/" v-slot="{ isActive }">
-									<Icon type="pubhubs-home" size="3xl" class="text-white mx-auto" @click="toggleMenu.toggleMenu()" ></Icon>
+									<Icon type="pubhubs-home" size="3xl" class="text-white mx-auto" @click="toggleMenu.toggleMenu()"></Icon>
 								</router-link>
 
 								<Line v-if="global.loggedIn && global.hasPinnedHubs" class="m-6 mt-8"></Line>
