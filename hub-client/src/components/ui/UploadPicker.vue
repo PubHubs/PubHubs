2cac73af2782b07d52eed7bbf6a824ac63413ce7
Omar Javed <o.javed@science.ru.nl>
2024-05-29 12:11:30 +0200

    Resolve "issues using safari on iphone"

--- hub-client/src/components/ui/UploadPicker.vue
+++ hub-client/src/components/ui/UploadPicker.vue
@@ -1,5 +1,5 @@
 <template>
-	<button class="flex flex-col items-center m-2" @click="$emit('click', $event)">
+	<button class="flex flex-col items-center m-2" @click="$emit('click')">
 		<Icon class="dark:text-white" type="upload"></Icon>
 		<p>{{ $t('message.upload_file') }}</p>
 	</button>
