cbb7780eeb2a80c27e53d1323d6f3463a8c357fb
Jan den Besten <jan.denbesten@ru.nl>
2024-05-30 14:26:37 +0200

    Discover room default

--- hub-client/src/components/rooms/DiscoverRooms.vue
+++ hub-client/src/components/rooms/DiscoverRooms.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="pl-6 pr-8 relative">
+	<div class="pl-6 pr-8 relative" @focusin="focus(true)" @focusout="focus(false)" @keydown.esc="focus(false)">
 		<Icon type="compass" class="absolute -ml-2 bg-white dark:bg-hub-background-2"></Icon>
-		<FilteredList :items="rooms.visiblePublicRooms" sortby="name" :placeholder="$t('rooms.discover')" :inputClass="'pl-6'" :listClass="'-mt-[17px] border rounded-md shadow-md'" :showCompleteList="false" @click="joinRoom($event)">
+		<FilteredList :items="rooms.visiblePublicRooms" sortby="name" :placeholder="$t('rooms.discover')" :inputClass="'pl-6'" :listClass="'-mt-[17px] border rounded-md shadow-md'" :showCompleteList="showList" @click="joinRoom($event)">
 			<template #item="{ item }">
 				<div class="flex justify-between">
 					<Icon :type="rooms.roomIsSecure(item.room_id) ? 'lock' : 'room'" class="flex-none mr-4 text-blue group-hover:text-black"></Icon>
@@ -14,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-	import { onMounted } from 'vue';
+	import { onMounted, ref } from 'vue';
 	import { useRouter } from 'vue-router';
 	import { TPublicRoom, useRooms } from '@/store/store';
 	import { usePubHubs } from '@/core/pubhubsStore';
@@ -23,10 +23,15 @@
 	const router = useRouter();
 	const pubhubs = usePubHubs();
 	const emit = defineEmits(['close']);
+	const showList = ref(false);
 
 	onMounted(async () => {
 		await rooms.fetchPublicRooms();
 	});
+
+	function focus(focus: boolean) {
+		showList.value = focus;
+	}
 
 	async function joinRoom(room: TPublicRoom) {
 		if (rooms.roomIsSecure(room.room_id)) {
