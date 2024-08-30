88103d9a212f38f7632ab009e39013b4627b6040
Harm van Stekelenburg <h.vanstekelenburg@cs.ru.nl>
2024-08-30 09:52:39 +0200

    Revert "Merge branch '862-matrix-js-sdk-import-error' into 'main'"

--- hub-client/vite.config.ts
+++ hub-client/vite.config.ts
@@ -19,12 +19,6 @@ export default defineConfig({
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: ['./test/setup-teardown-hooks.ts'],
-		// Remove this when https://github.com/matrix-org/matrix-js-sdk/issues/4287 is solved. This helps because matrix-js-sdk switched to ESM and node can't handle that: https://vitest.dev/config/#server-deps-inline
-		server: {
-      deps: {
-        inline: ['matrix-js-sdk'],
-      },
-    },
 	},
 	resolve: {
 		alias,
