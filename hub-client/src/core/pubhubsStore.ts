955c5ffd75c42b9c79a3c0a15e90e9b6dd78c0a7
Frans Lammers <f.lammers@cs.ru.nl>
2024-09-09 15:24:36 +0200

    Resolve "Search does not show all results when there are many"

--- hub-client/src/core/pubhubsStore.ts
+++ hub-client/src/core/pubhubsStore.ts
@@ -7,10 +7,10 @@ import { createNewPrivateRoomName, fetchMemberIdsFromPrivateRoomName, refreshPri
 import { hasHtml, sanitizeHtml } from '@/core/sanitizer';
 import { AskDisclosureMessage, YiviSigningSessionResult } from '@/lib/signedMessages';
 import { TMentions, TMessageEvent, TTextMessageEventContent } from '@/model/events/TMessageEvent';
-import { TSearchParameters, TSearchResult } from '@/model/model';
+import { TSearchParameters } from '@/model/model';
 import Room from '@/model/rooms/Room';
 import { RoomType, TPublicRoom, useConnection, User, useRooms, useUser } from '@/store/store';
-import { ContentHelpers, MatrixClient, MatrixError, MatrixEvent, Room as MatrixRoom, User as MatrixUser, MsgType } from 'matrix-js-sdk';
+import { ContentHelpers, ISearchResults, MatrixClient, MatrixError, MatrixEvent, Room as MatrixRoom, User as MatrixUser, MsgType } from 'matrix-js-sdk';
 import { ReceiptType } from 'matrix-js-sdk/lib/@types/read_receipts';
 
 let publicRoomsLoading: Promise<any> | null = null; // outside of defineStore to guarantee lifetime, not accessible outside this module
@@ -529,22 +529,30 @@ const usePubHubs = defineStore('pubhubs', {
 		/**
 		 * Performs search on content of given room
 		 *
-		 * @returns list of results as TSearchResult
+		 * @returns the promise of searchRoomEvents or an empty promise (when no term is given)
 		 */
-		async searchRoomEvents(term: string, searchParameters: TSearchParameters) {
-			if (!term || !term.length) return [];
+		async searchRoomEvents(term: string, searchParameters: TSearchParameters): Promise<ISearchResults> {
+			if (!term || !term.length) {
+				const emptySearchResult: ISearchResults = {
+					results: [],
+					highlights: [],
+				};
+				return emptySearchResult;
+			}
 
-			const response = await this.client.searchRoomEvents({ term: term, filter: { rooms: [searchParameters.roomId] } });
-			return response.results.map(
-				(result) =>
-					({
-						rank: result.rank,
-						event_id: result.context.ourEvent.event.event_id!,
-						event_type: result.context.ourEvent.event.type,
-						event_body: result.context.ourEvent.event.content?.body,
-						event_sender: result.context.ourEvent.event.sender,
-					}) as TSearchResult,
-			);
+			return await this.client.searchRoomEvents({ term: term, filter: { rooms: [searchParameters.roomId] } });
+		},
+
+		/**
+		 * Performs pagination on current searchResponse
+		 *
+		 * @returns the promise of searchRoomEvents or an empty promise (when no term is given)
+		 */
+		async backPaginateRoomEventsSearch(searchResponse: ISearchResults) {
+			if (searchResponse?.next_batch) {
+				return this.client.backPaginateRoomEventsSearch(searchResponse);
+			}
+			return searchResponse;
 		},
 
 		async hasUserJoinedHubFirstTime(): Promise<Object> {
