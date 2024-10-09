0b82864240f543e2fcb9695f5699aa56da2b1f41
Muhammet Temizkan <m.temizkan@science.ru.nl>
2024-10-09 12:37:15 +0200

    Remove draggable function

--- global-client/src/components/ui/HubMenu.vue
+++ global-client/src/components/ui/HubMenu.vue
@@ -3,27 +3,24 @@
 		<HubIcon class="text-black" :hub="hubs.currentHub" :active="true" :pinnable="global.loggedIn" @pin="global.addPinnedHub(hubs.currentHub, 0)" @click="toggleMenu.hideMenuAndSendToHub()"></HubIcon>
 		<Line v-if="global.hasPinnedHubs" class="m-6 mt-8"></Line>
 	</div>
-	<draggable :list="global.pinnedHubs" :item-key="'hubId'">
-		<template #item="{ element, index }">
-			<router-link :to="{ name: 'hub', params: { id: element.hubId } }" v-slot="{ isActive }">
-				<HubIcon
-					class="text-black"
-					v-if="global.loggedIn || element.hubId === hubs.currentHubId"
-					:hub="hubs.hub(element.hubId)"
-					:active="isActive"
-					:pinned="true"
-					@remove="global.removePinnedHub(index)"
-					@click="toggleMenu.hideMenuAndSendToHub()"
-				></HubIcon>
-			</router-link>
-		</template>
-	</draggable>
+	<div v-for="(element, index) in global.pinnedHubs" :key="element.hubId">
+		<router-link :to="{ name: 'hub', params: { id: element.hubId } }" v-slot="{ isActive }">
+			<HubIcon
+				class="text-ph-text"
+				v-if="global.loggedIn || element.hubId === hubs.currentHubId"
+				:hub="hubs.hub(element.hubId)"
+				:active="isActive"
+				:pinned="true"
+				@remove="global.removePinnedHub(index)"
+				@click="toggleMenu.hideMenuAndSendToHub()"
+			></HubIcon>
+		</router-link>
+	</div>
 </template>
 
 <script setup lang="ts">
-	import draggable from 'vuedraggable';
 	import { useGlobal, useHubs } from '@/store/store';
-	import { useToggleMenu } from '@/store/toggleGlobalMenu';
+import { useToggleMenu } from '@/store/toggleGlobalMenu';
 
 	const global = useGlobal();
 	const hubs = useHubs();
