67f403d918b53308cd67f21f4f8efcff2def0d83
Muhammet Temizkan <m.temizkan@science.ru.nl>
2024-10-15 15:49:59 +0200

    Room pill adjustments

--- hub-client/src/components/rooms/RoomPill.vue
+++ hub-client/src/components/rooms/RoomPill.vue
@@ -1,19 +1,17 @@
 <template>
-	<div class="flex max-w-[22rem] min-w-56 h-min rounded-md bg-white-middle text-black dark:bg-gray dark:text-white overflow-hidden" @mouseleave="expanded = false">
-		<div class="w-3 shrink-0 bg-hub-background-5"></div>
-		<div class="flex justify-between p-2 py-3 gap-2 w-full" :class="{ 'gap-4': expanded }">
+	<div class="flex max-w-[23rem] min-w-56 h-min rounded-md bg-white-middle text-black dark:bg-gray dark:text-white overflow-hidden cursor-pointer relative" @click="expandPillToggle()">
+		<div class="absolute left-0 top-0 h-full w-3 shrink-0 bg-hub-background-5 transition-all duration-1000 ease-in-out" :class="{ 'bg-notification w-full': joinedARoom }"></div>
+		<div class="flex justify-between p-2 pl-5 py-3 gap-2 w-full" :class="{ 'gap-4': expanded }">
 			<div class="flex gap-4 items-center">
 				<Icon :type="roomIsSecure ? 'shield' : 'speech_bubbles'" class="shrink-0"></Icon>
-				<div class="grid gap-0">
-					<H3 class="font-semibold line-clamp-1 m-0" :class="{ 'line-clamp-2': expanded }">{{ room?.name }}</H3>
-					<p class="text-xs line-clamp-1 italic" :class="{ 'line-clamp-3': expanded }">{{ room?.topic }}</p>
+				<div class="grid">
+					<H3 class="font-semibold overflow-hidden m-0 line-clamp-1 z-0" :class="{ 'line-clamp-3': expanded && !joinedARoom }">{{ room?.name }}</H3>
+					<p v-if="joinedARoom === false" class="text-xs line-clamp-1 italic" :class="{ 'line-clamp-3': expanded }">{{ room?.topic }}</p>
+					<p v-else class="text-base z-0">{{ t('rooms.joined') }}</p>
 				</div>
 			</div>
 			<div class="grid gap-2 items-center">
-				<div class="bg-hub-background rounded-md h-min hover:opacity-80">
-					<Icon type="chevron-down" class="hover:cursor-pointer transition-all" :class="{ 'rotate-180': expanded }" size="lg" @click="expandPillToggle()"></Icon>
-				</div>
-				<Icon type="join_room" size="lg" class="hover:cursor-pointer hover:opacity-80" :class="{ hidden: !expanded }" @click="joinRoom()"></Icon>
+				<Icon type="join_room" size="lg" class="hover:cursor-pointer hover:opacity-80" @click="joinRoom()"></Icon>
 			</div>
 		</div>
 	</div>
@@ -23,13 +21,14 @@
 	import { router } from '@/core/router';
 	import { ref } from 'vue';
 	import { usePubHubs } from '@/core/pubhubsStore';
-	import { useDialog } from '@/store/dialog';
 	import { useI18n } from 'vue-i18n';
 
 	const pubhubs = usePubHubs();
 	const { t } = useI18n();
 
-	let expanded = ref(false);
+	const expanded = ref(false);
+	const joinedARoom = ref(false);
+
 	const props = defineProps({
 		room: Object,
 		roomIsSecure: Boolean,
@@ -44,10 +43,10 @@
 			if (props.roomIsSecure === true) {
 				router.push({ name: 'secure-room', params: { id: props.room.room_id } });
 			} else {
-				const dialog = useDialog();
-				if (await dialog.okcancel(t('rooms.join_sure'), t('admin.name') + ': ' + props.room.name)) {
-					await pubhubs.joinRoom(props.room.room_id);
-				}
+				joinedARoom.value = true;
+				setTimeout(() => {
+					pubhubs.joinRoom(props.room?.room_id);
+				}, 3000);
 			}
 		}
 	}
