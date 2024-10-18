ec6f1ba5c4c8615417d51603a3e647a4962064b5
Bram Westerbaan <bram@westerbaan.name>
2024-10-18 16:46:49 +0200

    config update cli

--- pubhubs/src/cli/admin.rs
+++ pubhubs/src/cli/admin.rs
@@ -44,12 +44,16 @@ struct AdminContext {
 
 #[derive(clap::Subcommand, Debug)]
 enum Commands {
-    /// Get or temporarily change the configuration of one of the servers
+    /// Retrieves the current configuration,
+    /// or change it, using the `update` subcommand.
     Config(ConfigArgs),
 }
 
 #[derive(clap::Args, Debug)]
-pub struct ConfigArgs {}
+pub struct ConfigArgs {
+    #[command(subcommand)]
+    command: Option<ConfigCommands>,
+}
 
 impl ConfigArgs {
     fn run(self, ctx: AdminContext) -> Result<()> {
@@ -62,23 +66,7 @@ impl ConfigArgs {
     async fn run_async(self, ctx: AdminContext) -> Result<()> {
         let url = self.get_url(&ctx).await?;
 
-        let resp = api::query_with_retry::<api::admin::Info>(
-            &url,
-            &api::Signed::<api::admin::InfoReq>::new(
-                &*ctx.admin_key,
-                &api::admin::InfoReq {},
-                std::time::Duration::from_secs(10),
-            )
-            .unwrap(),
-        )
-        .await
-        .into_std()?;
-
-        let mut stdout = std::io::stdout().lock();
-
-        serde_json::to_writer_pretty(stdout, &resp.config)?;
-
-        Ok(())
+        self.command.unwrap_or_default().run(url, ctx).await
     }
 
     async fn get_url(&self, ctx: &AdminContext) -> Result<url::Url> {
@@ -95,5 +83,90 @@ impl ConfigArgs {
         let constellation = client::get_constellation(&ctx.config.phc_url).await?;
 
         Ok(constellation.url(ctx.server).clone())
+    }
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum ConfigCommands {
+    /// Retrieves the current config used by the server. [default]
+    Get(ConfigGetArgs),
+
+    /// Temporarily updates part of the server configuration.
+    ///
+    /// The changes are made in-memory, not on disk, and thus only persist across 'soft' server
+    /// restarts (where the `pubhubs serve` process does not actually restart) such as the ones
+    /// caused by discovery.
+    Update(ConfigUpdateArgs),
+}
+
+impl ConfigCommands {
+    async fn run(self, url: url::Url, ctx: AdminContext) -> Result<()> {
+        match self {
+            ConfigCommands::Get(args) => args.run(url, ctx).await,
+            ConfigCommands::Update(args) => args.run(url, ctx).await,
+        }
+    }
+}
+
+impl Default for ConfigCommands {
+    fn default() -> Self {
+        ConfigCommands::Get(ConfigGetArgs::default())
+    }
+}
+
+#[derive(clap::Args, Debug)]
+pub struct ConfigUpdateArgs {
+    /// Points to the part of the configuratio to change, e.g. '/phc/enc_key'.
+    #[arg(value_name = "POINTER")]
+    pub pointer: String,
+
+    /// The new JSON value to insert at `pointer`. FIXME: always parsed as JSON string
+    #[arg(value_name = "NEW_VALUE")]
+    pub new_value: serde_json::Value,
+}
+
+impl ConfigUpdateArgs {
+    async fn run(self, url: url::Url, ctx: AdminContext) -> Result<()> {
+        api::query_with_retry::<api::admin::UpdateConfig>(
+            &url,
+            &api::Signed::<api::admin::UpdateConfigReq>::new(
+                &*ctx.admin_key,
+                &api::admin::UpdateConfigReq {
+                    pointer: self.pointer,
+                    new_value: self.new_value,
+                },
+                std::time::Duration::from_secs(10),
+            )
+            .into_std()?,
+        )
+        .await
+        .into_std()?;
+
+        Ok(())
+    }
+}
+
+#[derive(clap::Args, Debug, Default)]
+pub struct ConfigGetArgs {}
+
+impl ConfigGetArgs {
+    async fn run(self, url: url::Url, ctx: AdminContext) -> Result<()> {
+        let resp = api::query_with_retry::<api::admin::Info>(
+            &url,
+            &api::Signed::<api::admin::InfoReq>::new(
+                &*ctx.admin_key,
+                &api::admin::InfoReq {},
+                std::time::Duration::from_secs(10),
+            )
+            .unwrap(),
+        )
+        .await
+        .into_std()?;
+
+        let stdout = std::io::stdout().lock();
+
+        serde_json::to_writer_pretty(stdout, &resp.config)?;
+
+        Ok(())
     }
 }
