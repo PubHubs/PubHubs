ae7707e814b8231564c0482d8ea7e696f2385f65
Omar Javed <omar.javed@ru.nl>
2025-01-29 10:18:23 +0100

    fixed document upload issue

--- hub-client/src/components/rooms/MessageFile.vue
+++ hub-client/src/components/rooms/MessageFile.vue
@@ -6,10 +6,24 @@
 </template>
 
 <script setup lang="ts">
-	// Components
-	import Icon from '../elements/Icon.vue';
-
+	import { useMatrixFiles } from '@/composables/useMatrixFiles';
+	import { SMI } from '@/dev/StatusMessage';
+	import { LOGGER } from '@/foundation/Logger';
 	import { TFileMessageEventContent } from '@/model/events/TMessageEvent';
 
-	defineProps<{ message: TFileMessageEventContent }>();
+	const { formUrlfromMxc } = useMatrixFiles();
+
+	const logger = LOGGER;
+
+	const props = defineProps<{ message: TFileMessageEventContent }>();
+
+	const url = getUrl();
+	if (!url) {
+		logger.error(SMI.ROOM_TIMELINEWINDOW, 'Url is missing in file message', props.message);
+	}
+
+	function getUrl() {
+		if (!props.message.url) return undefined;
+		return formUrlfromMxc(props.message.url);
+	}
 </script>
