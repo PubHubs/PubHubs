dd16671544f12d43f9da00b85c0844dc3c585330
Bram Westerbaan <bram@westerbaan.name>
2025-03-27 13:05:39 +0100

    (un)seal: use postcard and add non base64 encoded version

--- pubhubs/src/misc/crypto.rs
+++ pubhubs/src/misc/crypto.rs
@@ -14,14 +14,40 @@ pub fn random_alphanumeric() -> String {
         .collect()
 }
 
-/// Encodes and encrypts the given `obj` with additional associated data (or `b""` if `None`),
+/// Like [`seal`], but returns an urlsafe base64 encoded string.
 /// and returns it as urlsafe base64 string.  Use [`url_unseal`] to revert.
 pub fn url_seal<T: serde::Serialize>(
     obj: &T,
     key: &chacha20poly1305::Key,
     aad: impl AsRef<[u8]>,
 ) -> anyhow::Result<String> {
-    let plaintext = serde_json::to_vec(obj).context("serializing")?;
+    let buf: Vec<u8> = seal(obj, &key, aad)?;
+
+    Ok(Base64Url::encode_string(&buf))
+}
+
+/// Reverse of the [`url_seal`] operation.
+pub fn url_unseal<T: serde::de::DeserializeOwned>(
+    envelope: impl AsRef<str>,
+    key: &chacha20poly1305::Key,
+    aad: impl AsRef<[u8]>,
+) -> Result<T, crate::misc::error::Opaque> {
+    let buf = Base64Url::decode_vec(envelope.as_ref()).map_err(|_| crate::misc::error::OPAQUE)?;
+
+    unseal(&buf, &key, aad)
+}
+
+/// Encodes and encrypts the given `obj` with additional associated data (or `b""` if `None`).
+/// Use [`unseal`] to revert.
+///
+/// Uses a non self-describing encoding format for `T`, so [`seal`] is not suitable for long-lived
+/// data that might change.
+pub fn seal<T: serde::Serialize>(
+    obj: &T,
+    key: &chacha20poly1305::Key,
+    aad: impl AsRef<[u8]>,
+) -> anyhow::Result<Vec<u8>> {
+    let plaintext = postcard::to_stdvec(obj).context("serializing")?;
 
     let nonce = XChaCha20Poly1305::generate_nonce(&mut aead::OsRng);
     let ciphertext = XChaCha20Poly1305::new(key)
@@ -39,34 +65,40 @@ pub fn url_seal<T: serde::Serialize>(
     buf.extend_from_slice(&nonce);
     buf.extend_from_slice(&ciphertext);
 
-    Ok(Base64Url::encode_string(&buf))
+    Ok(buf)
 }
 
-/// Reverse of the [`url_seal`] operation.
-pub fn url_unseal<T: serde::de::DeserializeOwned>(
-    envelope: impl AsRef<str>,
+/// Reverse of the [`seal`] operation.
+pub fn unseal<T: serde::de::DeserializeOwned>(
+    envelope: impl AsRef<[u8]>,
     key: &chacha20poly1305::Key,
     aad: impl AsRef<[u8]>,
 ) -> Result<T, crate::misc::error::Opaque> {
-    let buf = Base64Url::decode_vec(envelope.as_ref()).map_err(|_| crate::misc::error::OPAQUE)?;
-
     let nonce_len: usize = chacha20poly1305::XNonce::len();
+    let envelope = envelope.as_ref();
 
-    if buf.len() < nonce_len {
+    if envelope.len() < nonce_len {
+        log::debug!("unseal: envelope does not contain nonce");
         return Err(crate::misc::error::OPAQUE);
     }
 
     let plaintext = XChaCha20Poly1305::new(key)
         .decrypt(
-            (&buf[..nonce_len]).into(),
+            (&envelope[..nonce_len]).into(),
             aead::Payload {
-                msg: &buf[nonce_len..],
+                msg: &envelope[nonce_len..],
                 aad: aad.as_ref(),
             },
         )
-        .map_err(|_| crate::misc::error::OPAQUE)?;
+        .map_err(|err| {
+            log::debug!("unseal: decrypting: {err}");
+            crate::misc::error::OPAQUE
+        })?;
 
-    serde_json::from_slice(&plaintext).map_err(|_| crate::misc::error::OPAQUE)
+    postcard::from_bytes(&plaintext).map_err(|err| {
+        log::debug!("unseal: decoding: {err}");
+        crate::misc::error::OPAQUE
+    })
 }
 
 /// Implements the `generic_array` version `1.2`
