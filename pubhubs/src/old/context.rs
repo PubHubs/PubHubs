c07e4a0ec1282520c4af3fbf95c754b87d290667
Bram Westerbaan <bram@westerbaan.name>
2025-04-17 18:33:37 +0200

    fixes #1247

--- pubhubs/src/old/context.rs
+++ pubhubs/src/old/context.rs
@@ -26,7 +26,7 @@ use crate::{
 
 pub struct Main {
     pub url: Urls,
-    pub bind_to: (String, u16),
+    pub bind_to: (Vec<String>, u16),
     pub connection_check_nonce: String,
 
     pub admins: HashSet<String>,
