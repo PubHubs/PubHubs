8b2bcc19445dced56e980f2eadf7f865360dfaf8
Floris Griep <f.griep@science.ru.nl>
2025-06-17 11:54:12 +0200

    Resolve "Onboarding - houserules mark-up"

--- hub-client/src/logic/store/hub-settings.ts
+++ hub-client/src/logic/store/hub-settings.ts
@@ -26,6 +26,38 @@ type HubSettingsState = {
 	_version: number;
 };
 
+export const toolbarSettings = {
+	bold: true,
+	italic: true,
+	header: true,
+	underline: true,
+	strikethrough: true,
+	mark: true,
+	superscript: true,
+	subscript: true,
+	quote: true,
+	ol: true,
+	ul: true,
+	link: true,
+	imagelink: false,
+	code: true,
+	table: true,
+	fullscreen: false,
+	readmodel: false,
+	htmlcode: true,
+	help: false,
+	undo: true,
+	redo: true,
+	trash: true,
+	save: false,
+	navigation: false,
+	alignleft: true,
+	aligncenter: true,
+	alignright: true,
+	subfield: true,
+	preview: true,
+};
+
 export const ALLOWED_HUB_ICON_TYPES = ['image/png', 'image/jpeg', 'image/jpg', 'image/svg+xml'];
 export const MAX_HUB_ICON_SIZE = 5000000; // ~5MB
 
