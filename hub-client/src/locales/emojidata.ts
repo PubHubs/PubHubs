fbf1797b5ddfb3b24c228708fd31d9c6769ba136
Frans Lammers <f.lammers@cs.ru.nl>
2025-07-02 10:21:55 +0200

    Resolve "Emoji's in the emojipicker are not shown on main"

---
+++ hub-client/src/locales/emojidata.ts
@@ -0,0 +1,11 @@
+import en from 'emojibase-data/en/data.json';
+import nl from 'emojibase-data/nl/data.json';
+// Add more languages here as needed
+
+const emojiData: Record<string, any[]> = {
+	en,
+	nl,
+	// Add more languages here as needed
+};
+
+export default emojiData;
