6c018a02464b4f36c17470df8a6bf06337bd30d3
Bram Westerbaan <bram@westerbaan.name>
2025-09-10 23:34:52 +0200

    mss: adds backwards compatibility to prevent a crash of PHC during the merge tomorrow

--- pubhubs/src/servers/constellation.rs
+++ pubhubs/src/servers/constellation.rs
@@ -45,6 +45,14 @@ impl Deref for Constellation {
     }
 }
 
+// NOTE: When adding a new field to the constellation make sure it has a default value in the first
+// version so that when the new version of PHC contacts the old versions of the transcryptor and
+// the authentication server, PHC will not crash on missing fields at the transcryptor and the
+// authentication server.
+//
+// (The converse is not necessary:  when an outdated authentication server and transcryptor
+// are running discovery against a freshly updated PHC, PHC's constellation will not be set
+// and will thus not cause the transcryptor or authentication server to crash.)
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
 pub struct Inner {
     pub transcryptor_url: url::Url,
@@ -64,10 +72,19 @@ pub struct Inner {
     /// `x_T x_PHC B`
     pub master_enc_key: elgamal::PublicKey,
 
+    #[serde(default = "default_global_client_url")]
+    // temporary default for backwards compatibility
     pub global_client_url: url::Url,
 
     /// pubhubs version
     pub ph_version: Option<String>,
+}
+
+/// Temporary default global client url for backwards compatibility
+fn default_global_client_url() -> url::Url {
+    "http://example.com/global-client-url-not-available"
+        .parse()
+        .unwrap()
 }
 
 impl Inner {
