05444f77ab3cc992439bc9752a35bd40b6c85537
Bram Westerbaan <bram@westerbaan.name>
2025-10-02 13:49:27 +0200

    please npx prettier

--- hub-client/src/client_config.ts
+++ hub-client/src/client_config.ts
@@ -1,6 +1,5 @@
-
 function adjustClientConfig() {
-	for (var key of ["HUB_URL", "PARENT_URL"]) {
+	for (var key of ['HUB_URL', 'PARENT_URL']) {
 		const vite_key = `VITE_${key}`;
 		if (vite_key in import.meta.env) {
 			_env[key] = import.meta.env[vite_key];
