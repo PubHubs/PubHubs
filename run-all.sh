17bd2fae515e7778a660678a7e535e95e4d6b8ff
Bram Westerbaan <bram@westerbaan.name>
2025-10-22 21:10:04 +0200

    have mask run all clean up its mess - fixes #1487

--- run-all.sh
+++ run-all.sh
@@ -5,6 +5,21 @@ SESSION=pubhubs
 # Start new session detached with first command
 tmux new-session -d -s $SESSION -n pubhubs 'mask run yivi'
 
+function cleanup() {
+	# Keep sending control + c ('C-c') to the tmux session until
+	# the session is gone (or there is some other error).
+	#
+	# Using 'tmux kill-session pubhubs' would be more ellegant here,
+	# but does not give 'docker run ... testhub ...' enough time to exit cleanly.
+	#
+	# Also, spamming control + c is what the developer would do manually anyways.
+	while tmux send-keys -t pubhubs C-c; do
+		sleep 0.1
+	done
+}
+
+trap 'cleanup' SIGINT EXIT
+
 sleep 0.2
 
 # Create splits for each remaining global command
