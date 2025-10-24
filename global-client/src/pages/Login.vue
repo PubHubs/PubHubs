0b1888bab7614e71cf33d2bc736849db6139590c
fgriep <f.griep@science.ru.nl>
2025-10-24 12:36:28 +0200

    fix login screen pubhubs logo

--- global-client/src/pages/Login.vue
+++ global-client/src/pages/Login.vue
@@ -60,7 +60,7 @@
 	import H1 from '@hub-client/components/elements/H1.vue';
 	import P from '@hub-client/components/elements/P.vue';
 	import InlineSpinner from '@hub-client/components/ui/InlineSpinner.vue';
-	import Logo from '@hub-client/components/ui/Logo.vue';
+	import Logo from '@global-client/components/ui/Logo.vue';
 
 	// Logic
 	import { CONFIG } from '@hub-client/logic/logging/Config';
