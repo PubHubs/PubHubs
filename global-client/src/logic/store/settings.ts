aa7d8530eaa79ab0b4e65350cb2e71a47d3f97b7
Jorrit van der Heide <jorrit@bw20.nl>
2025-10-29 15:45:24 +0100

    fix: remove unused files

--- global-client/src/logic/store/settings.ts
+++
@@ -1,3 +0,0 @@
-import { FeatureFlag, NotificationsPermission, Theme, TimeFormat, defaultSettings, i18nSettings, useSettings } from '@/../../hub-client/src/logic/store/settings.js';
-
-export { Theme, TimeFormat, NotificationsPermission, defaultSettings, FeatureFlag, useSettings, type i18nSettings };
