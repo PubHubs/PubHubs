4c26d514617810d722bcea0c86c796eceec61ca7
fgriep <f.griep@science.ru.nl>
2025-11-06 16:43:04 +0100

    hide headerFooter x overflow

--- hub-client/src/components/ui/HeaderFooter.vue
+++ hub-client/src/components/ui/HeaderFooter.vue
@@ -4,7 +4,7 @@
 			<div :class="['flex flex-col', bgBarMedium, isMobile ? 'h-[7.5rem] justify-center p-3' : 'h-[10rem] justify-start p-4']">
 				<slot name="header"></slot>
 			</div>
-			<div class="relative flex-1 overflow-y-auto">
+			<div class="relative flex-1 overflow-y-auto overflow-x-hidden">
 				<slot class="p-3 md:p-4"></slot>
 			</div>
 		</div>
