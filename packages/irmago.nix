ec06dd47c005a751dae7c9ca93eb2aa6e749df24
Jorrit van der Heide <bw20@noreply.codeberg.org>
2026-01-06 14:09:36 +0100

    update: nix flake & irma

--- packages/irmago.nix
+++ packages/irmago.nix
@@ -4,13 +4,13 @@
 }:
 pkgs.buildGoModule rec {
   pname = "irmago";
-  version = "0.19.0";
+  version = "0.19.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "privacybydesign";
     repo = "irmago";
     tag = "v${version}";
-    hash = "sha256-KhTaxMCoc9e6aJcGZ0bz4438ln4qky8Xr07UVmHiv7s=";
+    hash = "sha256-ym/w8I3KrintiK4GACZ54uDwTYAOdsyK18lPB14rkYg=";
   };
 
   vendorHash = "sha256-JUwzhngYf50PhknAladHrO/67z9UmLpr5f9LeLX5fI4=";
@@ -19,10 +19,11 @@ pkgs.buildGoModule rec {
 
   meta = {
     changelog = "https://github.com/privacybydesign/irmago/releases/tag/${src.tag}";
-    description = "IRMA CLI and server implementation in Go for privacy-preserving attribute issuance and verification";
+    description = "IRMA CLI and server implementation in Go";
     homepage = "https://docs.yivi.app/irma-cli";
     license = lib.licenses.asl20;
     mainProgram = "irmago";
+    
     maintainers = with lib.maintainers; [
       jorritvanderheide
     ];
