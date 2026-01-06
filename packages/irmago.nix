146e9d04756e8257b870e03fd677c7c3c93c80a4
Jorrit van der Heide <bw20@noreply.codeberg.org>
2026-01-06 14:13:22 +0100

    fix: annotate irmago file

--- packages/irmago.nix
+++ packages/irmago.nix
@@ -4,16 +4,18 @@
 }:
 pkgs.buildGoModule rec {
   pname = "irmago";
+  # To update, change the version number below and replace the hash
   version = "0.19.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "privacybydesign";
     repo = "irmago";
     tag = "v${version}";
+    # To get the hash of a new version, do `nix hash convert --hash-algo sha256 $(nix-prefetch-url --unpack https://github.com/privacybydesign/irmago/archive/refs/tags/v0.19.x.tar.gz 2>/dev/null)`
     hash = "sha256-ym/w8I3KrintiK4GACZ54uDwTYAOdsyK18lPB14rkYg=";
   };
 
-  vendorHash = "sha256-JUwzhngYf50PhknAladHrO/67z9UmLpr5f9LeLX5fI4=";
+  vendorHash = "sha256-JUwzhngYf50PhknAladHrO/67z9UmLpr5f9LeLX5fI4="; # This always stays the same
 
   subPackages = [ "irma" ];
 
