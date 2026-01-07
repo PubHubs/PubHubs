397954d8753304ee6adf7f84e4907b68052b7ad0
Jorrit van der Heide <bw20@noreply.codeberg.org>
2026-01-07 11:34:08 +0100

    chore: add tmux to devshell, update flake, update dependencies

--- flake.nix
+++ flake.nix
@@ -15,16 +15,19 @@
       ];
 
       # Helper to generate attrs for each system
-      forAllSystems = f: builtins.listToAttrs (
-        map (system: {
-          name = system;
-          value = f system;
-        }) systems
-      );
+      forAllSystems =
+        f:
+        builtins.listToAttrs (
+          map (system: {
+            name = system;
+            value = f system;
+          }) systems
+        );
     in
     {
       # Packages output
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
         in
@@ -35,29 +38,33 @@
       );
 
       # Development shells
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
         in
         {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustc # 1.91.1
-              cargo # 1.91.1
-              cargo-watch # 8.5.3
-              docker # 29.1.3
-              mask # 0.11.6
-              nodejs # 24.12.0
-              nodePackages.sass # 3.7.4
-              openssl # 3.6.0
-              pkg-config # 0.29.2
-              python3 # 3.13.11
-              sqlite # 3.51.1
-              tmux # 3.6a
-            ] ++ [
-              # Custom packages
-              (pkgs.callPackage ./packages/irmago.nix { }) # 0.19.1
-            ];
+            packages =
+              with pkgs;
+              [
+                rustc # 1.91.1
+                cargo # 1.91.1
+                cargo-watch # 8.5.3
+                docker # 29.1.3
+                mask # 0.11.6
+                nodejs # 24.12.0
+                nodePackages.sass # 3.7.4
+                openssl # 3.6.0
+                pkg-config # 0.29.2
+                python3 # 3.13.11
+                sqlite # 3.51.1
+                tmux # 3.6a
+              ]
+              ++ [
+                # Custom packages
+                (pkgs.callPackage ./packages/irmago.nix { }) # 0.19.1
+              ];
           };
         }
       );
