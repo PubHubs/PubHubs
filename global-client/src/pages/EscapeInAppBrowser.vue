84fd9d13726734b58eab420a3327b55341bc0d41
Bram Westerbaan <bram@westerbaan.name>
2026-02-04 15:39:47 +0100

    potential in-yivi-app-check: storage access check

--- global-client/src/pages/EscapeInAppBrowser.vue
+++ global-client/src/pages/EscapeInAppBrowser.vue
@@ -9,14 +9,22 @@
 		<li>secure content: {{ isSecureContext }}</li>
 		<li>title bar visible: {{ titleBarVisible }}</li>
 		<li>standalone: {{ standalone }}</li>
+		<li>storage access: {{ storageAccess }}</li>
 		<li><a href="https://inappdebugger.com">in-app debugger</a></li>
 	</ul>
 </template>
 
 <script setup lang="ts">
+	import { ref } from 'vue';
 	import { useRoute } from 'vue-router';
 
 	const isSecureContext = window.isSecureContext;
 	const titleBarVisible = navigator.windowControlsOverlay?.visible;
 	const standalone = navigator.standalone;
+
+	let storageAccess = ref(undefined);
+
+	document.hasStorageAccess().then((result) => {
+		storageAccess.value = result;
+	});
 </script>
