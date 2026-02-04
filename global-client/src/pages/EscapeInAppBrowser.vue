21d4140814f3d6077b5efbb67ca547124f280ed3
Bram Westerbaan <bram@westerbaan.name>
2026-02-04 16:27:58 +0100

    more in-yivi-app-check testing

--- global-client/src/pages/EscapeInAppBrowser.vue
+++ global-client/src/pages/EscapeInAppBrowser.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="w-full max-w-screen">
 		Please
-		<a :href="useRoute().query.next" target="_blank"> click here </a>
+		<strong><a :href="next" target="_blank"> click here </a></strong>
 		to proceed.
 	</div>
 
 	<ul>
-		<li>secure content: {{ isSecureContext }}</li>
+		<li>secure context: {{ isSecureContext }}</li>
 		<li>title bar visible: {{ titleBarVisible }}</li>
 		<li>standalone: {{ standalone }}</li>
 		<li>storage access: {{ storageAccess }}</li>
+		<li>referrer: {{ referrer }}</li>
 		<li><a href="https://inappdebugger.com">in-app debugger</a></li>
+		<li><a :href="'x-safari-' + next">Or open in Safari instead</a></li>
+		<li><a :href="'intent://' + nextWithoutScheme + '#Intent;scheme=https;end'">Or open using an intent link</a></li>
 	</ul>
 </template>
 
@@ -18,9 +21,13 @@
 	import { ref } from 'vue';
 	import { useRoute } from 'vue-router';
 
+	const next = useRoute().query.next;
+	const nextWithoutScheme = next.replace(/^[a-zA-Z0-9]*:\/\//, '');
+
 	const isSecureContext = window.isSecureContext;
 	const titleBarVisible = navigator.windowControlsOverlay?.visible;
 	const standalone = navigator.standalone;
+	const referrer = document.referrer;
 
 	let storageAccess = ref(undefined);
 
