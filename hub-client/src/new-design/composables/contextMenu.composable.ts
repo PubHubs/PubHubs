f4af2d3bb454972b994a8e96998bba37c7c2e4fa
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-02-04 15:57:33 +0100

    fix: update documentation & add touch events

--- hub-client/src/new-design/composables/contextMenu.composable.ts
+++ hub-client/src/new-design/composables/contextMenu.composable.ts
@@ -5,12 +5,22 @@ import type { MenuItem } from '@hub-client/new-design/stores/contextMenu.store';
 export function useContextMenu() {
 	const store = useContextMenuStore();
 
-	function openMenu(evt: MouseEvent | PointerEvent, items: MenuItem[], targetId: string | number | null = null) {
+	function openMenu(evt: MouseEvent | PointerEvent | TouchEvent, items: MenuItem[], targetId: string | number | null = null) {
 		evt.preventDefault();
 		evt.stopPropagation();
 
-		const x = (evt as MouseEvent).clientX ?? 0;
-		const y = (evt as MouseEvent).clientY ?? 0;
+		let x = 0;
+		let y = 0;
+
+		if ('touches' in evt && evt.touches.length > 0) {
+			// TouchEvent
+			x = evt.touches[0].clientX;
+			y = evt.touches[0].clientY;
+		} else if ('clientX' in evt) {
+			// MouseEvent or PointerEvent
+			x = evt.clientX;
+			y = evt.clientY;
+		}
 
 		store.open(items, Math.round(x), Math.round(y), targetId);
 	}
