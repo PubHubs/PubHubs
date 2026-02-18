d9e5ab8e859366c0feb1f87adb6c86fba2a014fe
Floris Griep <floris.griep@ru.nl>
2026-02-18 11:54:30 +0100

    copy workaround from SecuredRoomQr file

--- hub-client/src/components/forms/MessageInput.vue
+++ hub-client/src/components/forms/MessageInput.vue
@@ -148,7 +148,7 @@
 			<!-- Yivi signing qr popup -->
 			<div class="absolute bottom-[10%] left-1/2 min-w-64 -translate-x-1/2" v-show="messageInput.state.showYiviQR">
 				<Icon type="x" class="absolute right-2 z-10 cursor-pointer text-black" @click="messageInput.state.showYiviQR = false" />
-				<div v-if="messageInput.state.signMessage" :id="EYiviFlow.Sign"></div>
+				<div v-if="messageInput.state.signMessage" :id="EYiviFlow.Sign" ref="yivi-login-ref"></div>
 			</div>
 		</div>
 	</div>
@@ -156,7 +156,7 @@
 
 <script setup lang="ts">
 	// Packages
-	import { PropType, computed, onMounted, onUnmounted, ref, watch } from 'vue';
+	import { PropType, computed, onMounted, onUnmounted, onWatcherCleanup, ref, useTemplateRef, watch } from 'vue';
 	import { useI18n } from 'vue-i18n';
 	import { useRoute } from 'vue-router';
 
@@ -178,6 +178,8 @@
 	import { useMatrixFiles } from '@hub-client/composables/useMatrixFiles';
 
 	// Logic
+	import { LOGGER } from '@hub-client/logic/logging/Logger';
+	import { SMI } from '@hub-client/logic/logging/StatusMessage';
 	import { useMessageInput } from '@hub-client/logic/messageInput';
 	import { yiviFlow } from '@hub-client/logic/yiviHandler';
 
@@ -474,4 +476,61 @@
 	function setCaretPos(pos: { top: number; left: number }) {
 		caretPos.value = pos;
 	}
+
+	// START workaround for #1173, that iOS app links do not work in an iframe.
+	//
+	// NOTE: Please remove when e.g. https://github.com/privacybydesign/yivi-frontend-packages/pull/34 is merged
+	//
+	// Idea: we wait for the "Open Yivi app" anchor <a class="yivi-web-button-link" ...>
+	//       to be created using a MutationObserver, and add target="_top" attribute to it.
+	//
+	const yiviLoginRef = useTemplateRef('yivi-login-ref');
+
+	function onYiviNodeChange() {
+		LOGGER.trace(SMI.OTHER, "Changes to Yivi div's subtree");
+
+		const yiviAnchor: Node | undefined = yiviLoginRef.value!.querySelector('.yivi-web-button-link');
+
+		if (!yiviAnchor) {
+			LOGGER.trace(SMI.OTHER, "Yivi div changed, but no 'Open Yivi app' anchor was found.");
+			return;
+		}
+
+		if (!yiviAnchor.hasAttribute('href')) {
+			LOGGER.trace(SMI.OTHER, "'Open Yivi app' has no href attribute (yet)");
+			return;
+		}
+
+		if (yiviAnchor.getAttribute('href').startsWith('intent')) {
+			LOGGER.trace(SMI.OTHER, "'Open Yivi app' button uses intent link, so this is not iOS: not changing target");
+			return;
+		}
+
+		if (yiviAnchor.hasAttribute('target')) {
+			LOGGER.trace(SMI.OTHER, "'Open Yivi app' anchor's target was already set.");
+			return;
+		}
+
+		LOGGER.info(SMI.OTHER, 'Setting target="_top" on \'Open Yivi app\' anchor.');
+		yiviAnchor.setAttribute('target', '_top');
+	}
+
+	watch(yiviLoginRef, (yiviLoginEl) => {
+		LOGGER.trace(SMI.OTHER, 'The Yivi div itself changed');
+
+		// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+		const mutationObserver = new MutationObserver(onYiviNodeChange);
+
+		LOGGER.trace(SMI.OTHER, 'Connecting  MutationObserver to Yivi div for observing changes to its subtree');
+		mutationObserver.observe(yiviLoginEl, {
+			childList: true,
+			subtree: true,
+		});
+
+		onWatcherCleanup(() => {
+			LOGGER.trace(SMI.OTHER, 'Disconnecting Yivi div MutationObserver');
+			mutationObserver.disconnect();
+		});
+	});
+	// END workaround
 </script>
