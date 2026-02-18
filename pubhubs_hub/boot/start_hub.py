ebdaeece0ca04a83a0ebea3600efd60b051b708f
Bram Westerbaan <bram@westerbaan.name>
2026-02-18 17:09:12 +0100

    work on sqlite3 -> postgres

--- pubhubs_hub/boot/start_hub.py
+++ pubhubs_hub/boot/start_hub.py
@@ -5,6 +5,9 @@ import subprocess
 import threading
 import update_config
 import os
+import sys
+import shutil
+import time
 
 def main():
     parser = argparse.ArgumentParser(
@@ -23,6 +26,10 @@ def main():
                         help="Overwrites the hub server url used by the yivi app in the homeserver configuration")
     parser.add_argument("--global-client-url", default=None,
                         help="Overwrites the global client url in the homeserver configuration")
+    parser.add_argument("--replace-sqlite3-by-postgres",
+                        help="Replaces the configured sqlite3 database by a postgres database running inside this container.  Performs a migration, if necessary.",
+                        action=argparse.BooleanOptionalAction,
+                        default=True)
 
     Program(parser.parse_args()).run()
 
@@ -48,13 +55,14 @@ class Program:
         live_config_path = old_config_path[:-len("yaml")] + "live.yaml"
         os.environ["SYNAPSE_CONFIG_PATH"] = live_config_path
 
-        update_config.run(input_file=old_config_path,
+        uc = update_config.run(input_file=old_config_path,
                           output_file=live_config_path,
                           environment=self._args.environment,
                           hub_client_url=self._args.hub_client_url,
                           hub_server_url=self._args.hub_server_url,
                           hub_server_url_for_yivi=self._args.hub_server_url_for_yivi,
-                          global_client_url=self._args.global_client_url)
+                          global_client_url=self._args.global_client_url,
+                          replace_sqlite3_by_postgres=self._args.replace_sqlite3_by_postgres)
 
         self._waiter.add("yivi", subprocess.Popen(("/usr/bin/irma", 
                         "server",
@@ -68,19 +76,69 @@ class Program:
                         "-l", "0.0.0.0",
                         "-p", "8089",
                         "--client-listen-addr", "0.0.0.0",
-                        "--client-port", "8088")))
+                        "--client-port", "8088"),
+                        stderr=subprocess.DEVNULL,
+                        stdout=subprocess.DEVNULL))
 
         if self._args.environment == "development":
             os.putenv("AUTHLIB_INSECURE_TRANSPORT", "for_development_only_of_course")
+
+        if self._args.replace_sqlite3_by_postgres:
+            # find postres executable path
+            pg_data_dir = "/data/postgres"
+            pg_bindir = subprocess.run(('sudo', '-u', 'postgres',
+                                     'pg_config', '--bindir'),
+                                    check=True, capture_output=True).stdout.strip().decode('utf-8')
+
+            fresh_db = self.prepare_pg_data_dir(pg_data_dir=pg_data_dir, pg_bindir=pg_bindir)
+
+            # run postgres
+            self._waiter.add("postgres", 
+                             subprocess.Popen(('sudo', '-u', 'postgres',
+                                               os.path.join(pg_bindir, "postgres"),
+                                               '-D', pg_data_dir),
+                                               stdout=subprocess.DEVNULL,
+                                               stderr=subprocess.DEVNULL,
+                                               ))
+            countdown = 5
+            while subprocess.run(("sudo", "-u", "postgres", "pg_isready", "-q")).returncode != 0:
+                eprint(f"waiting {countdown} seconds for the postgres server to come up")
+                time.sleep(1)
+                countdown -= 1
+                if countdown == 0:
+                    raise RuntimeError("postgres server did not start properly; the reason might be in the logs above")
+
+            if fresh_db:
+                subprocess.run(("sudo", "-u", "postgres", "createuser", "synapse"), check=True)
+                subprocess.run(("sudo", "-u", "postgres", 
+                                "createdb", "hub",
+                                "--encoding=UTF8",
+                                "--locale=C",
+                                "--template=template0",
+                                "--owner=synapse"), check=True)
+
 
         self._waiter.add("synapse", subprocess.Popen(("/start.py",)))
 
         self._waiter.wait()
 
+    def prepare_pg_data_dir(self, pg_data_dir, pg_bindir):
+        if os.path.exists(pg_data_dir):
+            return False
 
+        os.mkdir(pg_data_dir)
+
+        subprocess.run(("sudo", "-u", "postgres", 
+                        os.path.join(pg_bindir, "initdb"), pg_data_dir),
+                       check=True)
+
+        return True
+
+
+# wait() waits for any of the processes added to Waiter to quit
 class Waiter:
     def __init__(self):
-        self._barrier = threading.Barrier(2)
+        self._barrier = threading.Barrier(2) 
 
     def add(self, name, process):
         # daemon=True makes sure that this thread does not keep the whole process alive when the main thread exists
