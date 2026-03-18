642145067f6762f9cefcf460d122a48e8a4508da
Jan den Besten <info@jandenbesten.net>
2026-03-18 14:57:49 +0100

    Replaced SortingMenu with default ActionMenu

--- hub-client/src/components/rooms/forum/SortPostsButton.vue
+++ hub-client/src/components/rooms/forum/SortPostsButton.vue
@@ -1,11 +1,10 @@
 <template>
 	<div class="flex flex-row items-center gap-1 wrap-break-word">
-		<Button @click="sortingStore.openMenu()" aria-label="Open sorting menu" icon="arrows-down-up">Sort</Button>
-		<SortingMenu v-if="sortingStore.isMenuOpen" />
 		<ActionMenu icon="arrows-down-up">
 			<ActionMenuItem v-for="option in sortOptions" :key="option.key" @click="sortingStore.toggleOption(option.key)" class="flex items-center gap-2">
 				<Icon :type="option.icon"></Icon>
 				{{ option.label }}
+				<Icon :type="sortingStore.getDirectionIcon(option.key)"></Icon>
 			</ActionMenuItem>
 		</ActionMenu>
 	</div>
@@ -13,15 +12,12 @@
 
 <script setup lang="ts">
 	// Components
-	import SortingMenu from '@hub-client/components/rooms/forum/SortingMenu.vue';
 	import ActionMenu from '@hub-client/components/ui/ActionMenu.vue';
 	import ActionMenuItem from '@hub-client/components/ui/ActionMenuItem.vue';
 
 	// Stores
 	import { useSortingStore } from '@hub-client/stores/forum/sortingStore';
 	import { sortOptions } from '@hub-client/stores/forum/sortingStore';
-
-	import Button from '@hub-client/new-design/components/Button.vue';
 
 	const sortingStore = useSortingStore();
 </script>
