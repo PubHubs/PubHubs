608731554c0881f97edce1a1c7fb8f489435e24a
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-04-01 14:23:09 +0200

    fix: cursor on room/hub title

--- hub-client/src/pages/Room.vue
+++ hub-client/src/pages/Room.vue
@@ -10,11 +10,7 @@
 				<div v-if="rooms.currentRoom" class="relative flex min-w-0 flex-1 items-center gap-3 overflow-hidden" data-testid="roomtype">
 					<Icon v-if="!notPrivateRoom()" type="caret-left" data-testid="back" class="cursor-pointer" @click="router.push({ name: 'direct-msg' })" />
 					<Icon v-else-if="notPrivateRoom()" :type="rooms.currentRoom.isSecuredRoom() ? 'shield' : 'chats-circle'" />
-					<div
-						class="group relative"
-						:class="!rooms.currentRoom.isDirectMessageRoom() && 'hover:cursor-pointer'"
-						v-context-menu="!rooms.currentRoom.isDirectMessageRoom() ? (evt: any) => openMenu(evt, [{ label: t('menu.copy_room_url'), icon: 'copy', onClick: () => copyRoomUrl() }]) : undefined"
-					>
+					<div class="group relative" v-context-menu="!rooms.currentRoom.isDirectMessageRoom() ? (evt: any) => openMenu(evt, [{ label: t('menu.copy_room_url'), icon: 'copy', onClick: () => copyRoomUrl() }]) : undefined">
 						<H3 class="text-on-surface flex">
 							<TruncatedText class="font-headings font-semibold">
 								<PrivateRoomHeader v-if="room!.isPrivateRoom()" :room="room!" :members="room!.getOtherJoinedAndInvitedMembers()" />
