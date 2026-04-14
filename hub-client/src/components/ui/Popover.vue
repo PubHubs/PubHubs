71d7b067964c2f746089a4f25210572fa64105e8
Jan den Besten <info@jandenbesten.net>
2026-04-14 11:35:07 +0200

    Popover can now also popover under element (depending on position)

--- hub-client/src/components/ui/Popover.vue
+++ hub-client/src/components/ui/Popover.vue
@@ -1,7 +1,9 @@
 <template>
 	<div
+		ref="popover"
 		v-click-outside="close"
 		class="z-40 rounded-md"
+		:style="style"
 		role="toolbar"
 	>
 		<IconButton
@@ -16,11 +18,18 @@
 </template>
 
 <script lang="ts" setup>
+	// Packages
+	import { nextTick, onMounted, ref } from 'vue';
+
+	// Components
+	import IconButton from '@hub-client/new-design/components/IconButton.vue';
+
 	withDefaults(defineProps<Props>(), {
 		showClosingCross: false,
 	});
-
 	const emit = defineEmits(['close']);
+	const popover = ref<HTMLElement | null>(null);
+	const style = ref('');
 
 	window.addEventListener(
 		'keydown',
@@ -35,6 +44,19 @@
 	type Props = {
 		showClosingCross?: boolean;
 	};
+
+	onMounted(() => {
+		nextTick(() => {
+			const split = Math.floor(window.innerHeight / 2);
+			const popoverY = popover.value?.getBoundingClientRect().top ?? 0;
+			const popoverSize = popover.value?.clientHeight ?? 0;
+			let adjust = 0;
+			if (popoverY < split) {
+				adjust = popoverSize;
+				style.value += 'top:' + adjust + 'px;';
+			}
+		});
+	});
 
 	async function close() {
 		emit('close');
