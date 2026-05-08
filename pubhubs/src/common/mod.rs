c6efbe497c404f67ef8ae541228e3594103eb749
Bram Westerbaan <bram@westerbaan.name>
2026-05-08 17:42:10 +0200

    minor change to test gitlab rust cache

--- pubhubs/src/common/mod.rs
+++ pubhubs/src/common/mod.rs
@@ -1,4 +1,4 @@
-//! What was common between the binary and the ABI (which is longer used).
+//! What was common between the binary and the ABI (which is no longer used).
 
 pub mod elgamal;
 pub mod secret;
