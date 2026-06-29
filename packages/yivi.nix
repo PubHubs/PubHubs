1583dd3811ebc67b3866f71de44d625cbe07cafa
Jorrit van der Heide <jorrit+git@bw20.nl>
2026-06-29 16:21:23 +0200

    chore: update irmago and rename to yivi

--- packages/yivi.nix
+++ packages/yivi.nix
@@ -5,26 +5,28 @@
 pkgs.buildGoModule rec {
   pname = "irmago";
   # To update, change the version number below and replace the hash
-  version = "0.19.2";
+  version = "1.0.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "privacybydesign";
     repo = "irmago";
     tag = "v${version}";
-    # To get the hash of a new version, do `nix hash convert --hash-algo sha256 $(nix-prefetch-url --unpack https://github.com/privacybydesign/irmago/archive/refs/tags/v0.19.x.tar.gz 2>/dev/null)`
-    hash = "sha256-CJDKwBL07Ok8MsZiu8WDTFJbsikgGHGt/E3k2NbXFyk=";
+    # To get the hash of a new version, do `nix hash convert --hash-algo sha256 $(nix-prefetch-url --unpack https://github.com/privacybydesign/irmago/archive/refs/tags/v1.x.x.tar.gz 2>/dev/null)`
+    hash = "sha256-ZV8H4WNxIIfkk5KYQdbbOEfyjI1vbI+WkdHBH6P4010=";
   };
 
-  vendorHash = "sha256-JUwzhngYf50PhknAladHrO/67z9UmLpr5f9LeLX5fI4="; # This always stays the same
+  vendorHash = "sha256-d2whPQmcUAFGbuSztBB7yZTuAlQyIQEnWGDnwbcPMxE=";
 
-  subPackages = [ "irma" ];
+  subPackages = [ "yivi" ];
+
+  doCheck = false;
 
   meta = {
     changelog = "https://github.com/privacybydesign/irmago/releases/tag/${src.tag}";
     description = "IRMA CLI and server implementation in Go";
     homepage = "https://docs.yivi.app/irma-cli";
     license = lib.licenses.asl20;
-    mainProgram = "irma";
+    mainProgram = "yivi";
 
     maintainers = with lib.maintainers; [
       jorritvanderheide
